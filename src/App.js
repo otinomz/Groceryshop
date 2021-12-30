b05c673882863733c77f134d2f009e5b6aaf0780
otinomz <awesomeotinomz@gmail.com>
2021-12-30 15:32:09 +0100

    patch method added

--- src/App.js
+++ src/App.js
@@ -55,12 +55,23 @@ const App = () => {
   } 
  
   // handling change
-  const handleCheck = (id) => {
-    const listItems = items.map (
-      (item) => item.id === id ? { ...item, checked: !item.checked } : item
-    )
-    setItems(listItems)
+  const handleCheck = async (id) => {
+    const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
+    setItems(listItems);
+
+    const myItem = listItems.filter((item) => item.id === id)
+    const updateOptions = {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ checked: myItem[0].checked })
+    };
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, updateOptions);
+    if (result) setFetchError(result);
   }
+
 
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id)
